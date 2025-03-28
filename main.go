95a35f1dc164092aef7130a41f5c1a49d8a2cafb
otavio lima <otavio.p.lima@aluno.senai.br>
2025-03-28 08:44:46 -0300

    verificação de senha

--- main.go
+++ main.go
@@ -2,16 +2,20 @@ package main
 import "fmt"
 
 func main(){
-	var a int 
+	var usuario = "admin"
+	var senha = 1234
+
+	
+	var a string
 	var b int
-	fmt.Println("Escolha um numero:")
+	fmt.Println("Escolha um nome de usario:")
 	fmt.Scan(&a)
-	fmt.Println("Escolha outro numero:")
+	fmt.Println("Escolha uma senha em numeros:")
 	fmt.Scan(&b)
 
-	fmt.Println("A soma dos numeros é: ", a + b)
-	fmt.Println("A subtração dos numeros é: ", a - b)
-	fmt.Println("A divisão dos numeros é: ", a / b)
-	fmt.Println("A multiplicação dos numeros é: ", a * b)
-	fmt.Println("O resto da divisão é:", a % b)
+	if a == usuario && b == senha{
+		fmt.Println("Você acessou o programa!")
+	}else {
+		fmt.Println("Acesso negado")
+	}
 }
