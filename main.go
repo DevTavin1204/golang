8f8438f73f54401bc59d5ba0bb8c115aa1a22abf
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-03 08:20:23 -0300

    vetor

--- main.go
+++ main.go
@@ -2,20 +2,10 @@ package main
 import "fmt"
 
 func main(){
-	var usuario = "admin"
-	var senha = 1234
-
-	
-	var a string
-	var b int
-	fmt.Println("Escolha um nome de usario:")
-	fmt.Scan(&a)
-	fmt.Println("Escolha uma senha em numeros:")
-	fmt.Scan(&b)
-
-	if a == usuario && b == senha{
-		fmt.Println("Você acessou o programa!")
-	}else {
-		fmt.Println("Acesso negado")
-	}
+    var ages = [4]int{17, 16, 20, 40}
+	nomes := [4]string{"fabiano", "heitor", "maria", "otavio"}
+	fmt.Println(ages)
+	fmt.Println(nomes)
+	nomes[3] = "Fabricio"
+	fmt.Println(nomes)
 }
