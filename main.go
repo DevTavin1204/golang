e19597de85ea9a8d3713ac8c0a1e64a5b2a47bf4
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-04 13:04:17 -0300

    laço no vetor

--- main.go
+++ main.go
@@ -20,4 +20,30 @@ func main(){
    sort.Strings(names)
    fmt.Println(names)
    fmt.Println(sort.SearchStrings(names, "Otávio"))
+   x :=0
+
+   for x < 5 {
+      fmt.Println(x)
+      x++
+   }
+
+   for i:=0; i <5; i++{
+      fmt.Println("For 2: ", i)
+   }
+   for i:=0; i <len(names); i++{
+      fmt.Println(names[i])
+   }
+   for index, value := range names{
+      fmt.Println("O indice é:", index, "e o valor é", value)
+   }
+   for index, value := range ages{
+      fmt.Println("o indice é:", index, " e o valor é:", value)
+   }
+
+
+   superherois := []string{"Deadpool", "Homem aranha", "batman"}
+
+   for index, value := range superherois{
+      fmt.Println("O numero do heroi: ", index, "O nome do heroi: ", value)
+   }
 }
