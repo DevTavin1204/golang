86c7c3ab265bbaec011f7835f8474f4e829f5e7f
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-10 08:24:11 -0300

    soma no vetor

--- main.go
+++ main.go
@@ -1,49 +1,20 @@
 package main
-import ("fmt"
-         "strings"
-         "sort"
-		)
+import ("fmt")
 func main(){
-   greeting := "Hello my friends"
-   fmt.Println(strings.Contains(greeting, "friends"))
-   fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-   fmt.Println(strings.ToUpper(greeting))
-   fmt.Println(strings.Index(greeting, "my"))
-   fmt.Println(strings.Split(greeting, "friends"))
+  var numeros = [5]int{}
+  var soma int
 
-   ages := []int {50, 80, 10}
-   sort.Ints(ages)
-   fmt.Println(ages)
-   index := sort.SearchInts(ages, 80)
-   fmt.Println(index)
-   names := []string{"Otávio", "Denise", "Fabricio"}
-   sort.Strings(names)
-   fmt.Println(names)
-   fmt.Println(sort.SearchStrings(names, "Otávio"))
-   x :=0
+ fmt.Println("escolha o primeiro numero:")
+ fmt.Scan(&numeros[0])
+ fmt.Println("escolha o segundo numero:")
+ fmt.Scan(&numeros[1])
+ fmt.Println("escolha o terceiro numero:")
+ fmt.Scan(&numeros[2])
+ fmt.Println("escolha o quarto numero:")
+ fmt.Scan(&numeros[3])
+ fmt.Println("escolha o quinto numero:")
+ fmt.Scan(&numeros[4])
 
-   for x < 5 {
-      fmt.Println(x)
-      x++
-   }
-
-   for i:=0; i <5; i++{
-      fmt.Println("For 2: ", i)
-   }
-   for i:=0; i <len(names); i++{
-      fmt.Println(names[i])
-   }
-   for index, value := range names{
-      fmt.Println("O indice é:", index, "e o valor é", value)
-   }
-   for index, value := range ages{
-      fmt.Println("o indice é:", index, " e o valor é:", value)
-   }
-
-
-   superherois := []string{"Deadpool", "Homem aranha", "batman"}
-
-   for index, value := range superherois{
-      fmt.Println("O numero do heroi: ", index, "O nome do heroi: ", value)
-   }
-}
+soma = numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]
+ fmt.Println("A soma dos numeros é:", soma)
+ }
