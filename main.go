381c6cfbdad0c33bd2b83a94f6c9a87b84b77d7e
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-10 10:13:36 -0300

    soma de idades

--- main.go
+++ main.go
@@ -5,31 +5,15 @@ import (
 	
 )
 func main(){
-   age := 45
-   fmt.Println(age <= 50)
-   fmt.Println(age >= 50)
-   fmt.Println(age == 50)
-   fmt.Println(age != 50)
+     var age int
+     fmt.Println("qual é sua idade?")
+     fmt.Scan(&age)
 
-   if age < 30{
-      fmt.Println("menor que 30 anos")
-}else if age < 40{
-   fmt.Println("menor que 40 anos")
-}else {
-   fmt.Println("Não é menor que 40 anos")
-}
-
-names := []string{"Otávio", "Denise", "Fabricio", "Gojo", "Neymar"}
-
-for index, value := range names{
-   if index == 0{
-      fmt.Println("continue após a posição", index, "e valor", value)
-      continue
-   }
-   if index > 3{
-       fmt.Println("sair após", index)
-       break
-   }
-   fmt.Println("valor:", value)
-}
+     if age < 18 {
+      fmt.Println("Você é menor de idade")
+     }else if age <= 60{
+      fmt.Println("Você é adulto")
+     }else {
+      fmt.Println("Você é idoso")
+     }
  }
