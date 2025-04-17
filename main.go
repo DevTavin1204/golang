c2741d3075166719073981d28907b43c4ae37554
otavio lima <otavio.p.lima@aluno.senai.br>
2025-04-17 08:38:41 -0300

    funçao com 2 return

--- main.go
+++ main.go
@@ -1,39 +1,28 @@
 package main
 
 import ("fmt")
-
-var saldo int = 10000
-var resposta int
-
-func sacar(valorSaque int) int{
-fmt.Println("quanto você deseja sacar?")
-    fmt.Scan(&valorSaque)
-    if valorSaque > 10000{
-      fmt.Println("Saldo insuficiente")
-  } 
-resposta = saldo - valorSaque
-return resposta
+ func dividir(dividendo int, divisor int) (int, string){
+    if divisor == 0 {
+      return 0, "Erro na divisão por zero"
+    }
+    return dividendo / divisor, ""
+ }
+func operacaoBasica(a int, b int)(int, int, int){
+  soma := a + b 
+  multiplicacao := a * b
+  subtracao := a - b
+  return soma, multiplicacao, subtracao
 }
-func depositar(valorDeposito int) int{
-  fmt.Println("quanto você deseja depositar?")
-    fmt.Scan(&valorDeposito)
-  resposta = saldo + valorDeposito
-  return resposta
-}
-func verSaldo(valorConta int){
-  fmt.Println("O seu saldo é:", resposta)
-}
+ func main(){
+   resultado, erro := dividir(10,2)
 
-func main(){
-  escolha := "sacar || depositar" 
-  fmt.Println("Oque você quer fazer? sacar/depositar")
-  fmt.Scan(&escolha)
-
-  if escolha == "sacar"{
-    sacar(saldo)
-
-  }else if escolha == "depositar"{
-    depositar(saldo)
-  }
-  verSaldo(resposta)
-}
+   if erro != ""{
+    fmt.Println(erro)
+   }else {
+    fmt.Println("O resultado da divisão é:", resultado)
+   }
+   soma, mult, sub := operacaoBasica(10,2)
+   fmt.Println(soma)
+   fmt.Println(mult)
+   fmt.Println(sub)
+ }
